9623e70b1d7805c2ca3717c9bff29c932411b01b
AdvanceCPPRepo <109949057+AdavanceCPP@users.noreply.github.com>
2022-07-26 10:36:27 +0530

    Add files via upload

---
+++ Day2/Inlinefunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void Add(int, int);
+
+int main()
+{
+    Add(10,20);
+    return 0;
+}
+
+void Add(int x, int y)
+{
+    cout <<"sum =" << x+y << endl;
+}
+
